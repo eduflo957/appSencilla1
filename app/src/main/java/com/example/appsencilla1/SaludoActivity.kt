065c23a9137e0722b6bc0fd03f8d001b54e19203
BungaEdu <eduardoflores.9595@gmail.com>
2022-10-26 00:55:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appsencilla1/SaludoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appsencilla1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaludoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+    }
+}
